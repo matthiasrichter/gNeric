982c7c8d6061799874a334d96c9bf3fd95b01cb8
Matthias Richter <Matthias.Richter@scieq.net>
2016-09-11 09:46:50 +0200

    Adding example how to use boost::mpl::apply to create mixin
    Furthermore, minor update to make sure that the mixing class mode
    is dynamically chosen rather than using a number which is fixed at
    compile time.

--- dynamic_mixin.cxx
+++ dynamic_mixin.cxx
@@ -13,6 +13,7 @@
 #include <boost/mpl/map.hpp>
 #include <boost/mpl/pair.hpp>
 #include <boost/mpl/int.hpp>
+#include <boost/mpl/apply.hpp>
 
 #include "composite_factory.h"
 #include "mixinclass.h"
@@ -32,10 +33,31 @@ std::shared_ptr< Interface > create( int property )
 
 int main()
 {
-  std::shared_ptr<Interface> dyninterface1(create( 0x1 | 0x4 ));
+  volatile uint16_t mode = 0x1 | 0x4;
+  std::shared_ptr<Interface> dyninterface1(create( mode ));
   std::cout << "======== dynamic interface 1 ======================" << std::endl;
   dyninterface1->print();
-  std::shared_ptr<Interface> dyninterface2(create( 0x2 ));
+  mode = 0x2;
+  std::shared_ptr<Interface> dyninterface2(create( mode ));
   std::cout << "======== dynamic interface 2 ======================" << std::endl;
   dyninterface2->print();
+
+  std::cout << "======== mixin with boost mpl apply  ==============" << std::endl;
+  // define a data type for dec on top of base funtionality
+  typedef dec<BaseFunctionality<int>> dec_t;
+
+  // define an application of hex on top of dec_t as new data type
+  // the Nth occurrence of the unnamed placeholder '_' is replaced
+  // by the Nth argument, in this example '_1' could also be used
+  using namespace boost::mpl::placeholders;
+  typedef boost::mpl::apply1< hex<_>, dec_t >::type hexdec_t;
+
+  // make objects from the data types and print the structure
+  dec_t deco;
+  hexdec_t hexdeco;
+  std::cout << "one-level mixin (explicitely declared):" << std::endl;
+  deco.print();
+  std::cout << "two-level mixin (boost mpl apply):" << std::endl;
+  hexdeco.print();
+
 }
